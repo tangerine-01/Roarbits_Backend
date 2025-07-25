6716b61dfbf16a43914d11c67442d7bc4b279e2c
bugloss12 <ataraxia@kau.kr>
2025-07-25 10:46:07 +0900

    gitginore 중복 수정

--- src/main/java/roarbits/notification/entity/UserInterest.java
+++ src/main/java/roarbits/notification/entity/UserInterest.java
@@ -1,4 +1,28 @@
 package roarbits.notification.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "user_interest")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserInterest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private String interestType;
+    private Long interestTargetId;
+    private boolean enabled;
+
+    //알림수신 여부 변경 메서드
+    public void setEnabled(boolean enabled) {
+        this.enabled = !this.enabled;
+    }
 }
--- src/main/java/roarbits/notification/service/UserInterestService.java
+++ src/main/java/roarbits/notification/service/UserInterestService.java
@@ -1,4 +1,49 @@
 package roarbits.notification.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import roarbits.notification.dto.UserInterestRequestDto;
+import roarbits.notification.dto.UserInterestResponseDto;
+import roarbits.notification.entity.UserInterest;
+import roarbits.notification.repository.UserInterestRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class UserInterestService {
+
+    private final UserInterestRepository repository;
+
+    // 관심 알림 설정 등록 또는 수정
+    public UserInterestResponseDto saveOrUpdateInterest(UserInterestRequestDto dto) {
+        UserInterest entity = repository.findByUserIdAndInterestTypeAndInterestTargetId(
+                        dto.getUserId(), dto.getInterestType(), dto.getInterestTargetId())
+                .orElse(UserInterest.builder()
+                        .userId(dto.getUserId())
+                        .interestType(dto.getInterestType())
+                        .interestTargetId(dto.getInterestTargetId())
+                        .build());
+
+        entity.setEnabled(dto.isEnabled());
+        return UserInterestResponseDto.fromEntity(repository.save(entity));
+    }
+
+    // 사용자 관심 목록 조회
+    @Transactional(readOnly = true)
+    public List<UserInterestResponseDto> getUserInterests(Long userId) {
+        List<UserInterest> list = repository.findByUserId(userId);
+        return list.stream()
+                .map(UserInterestResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    // 관심 알림 삭제
+    public void deleteInterest(Long id) {
+        repository.deleteById(id);
+    }
 }
