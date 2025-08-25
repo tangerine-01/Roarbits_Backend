4695d7b7ae6b2e0359f900ad4e00e6555ce45ff9
bugloss12 <ataraxia@kau.kr>
2025-08-25 18:09:19 +0900

    username 오류

--- src/main/java/roarbits/user/repository/UserRepository.java
+++ src/main/java/roarbits/user/repository/UserRepository.java
@@ -8,6 +8,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     Optional<User> findByEmail(String email);
     Optional<User> findByEmailIgnoreCase(String email);
-
-    Optional<User> findByUsername(String username);
 }
